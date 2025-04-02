ae4951cc7ad1e6895c2157930520b9322e6cffc3
Luiz Fernando <fernandinho.manon@gmail.com>
2025-04-02 07:57:20 -0300

    Criado duas pastas para separar as aulas de JavaScript

---
+++ JavaScript - Básico/aula-07/operadores_booleanos.js
@@ -0,0 +1,10 @@
+// Operadore Booleanos: Comparações
+
+const numero1 = 10;
+const numero2 = 12;
+
+console.log(numero1 == numero2);
+console.log(numero1 === numero2);
+console.log(numero1 == "10");
+
+console.log(numero1 !== 10);
