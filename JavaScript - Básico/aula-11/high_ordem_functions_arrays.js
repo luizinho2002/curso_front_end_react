56e2801f1f14f30fee1ad1d31bdcd1aeeba464a3
Luiz Fernando <fernandinho.manon@gmail.com>
2025-05-14 12:51:22 -0300

    Vendo sobre o método find em array

---
+++ JavaScript - Básico/aula-11/high_ordem_functions_arrays.js
@@ -0,0 +1,13 @@
+// * FUNÇÕES DE ALTA ORDEM DE ARRAYS
+
+// 1. Array.prototype.forEach()
+
+const numeros = [40, 34, 67, 89, 23, 10];
+
+numeros.forEach((item, index, arrayCompleto) => {
+  console.log(index, item, arrayCompleto);
+});
+
+// 2. Array.prototype.fina()
+
+
